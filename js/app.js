8f76ea97266b3a3b2784a2e32d7fd7089dc39547
jessemadsen1 <49500141+jessemadsen1@users.noreply.github.com>
2020-10-31 08:23:58 -0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,85 @@
+Vue.component('semester', {
+
+  props: ['course','semester_name'],
+  template: `    
+  <section v-if = "course.SEMESTER === semester_name && course.COMPLETED == true">
+  <h6>{{course.CRS}} {{course.TITLE}}</h6>
+</section>`,
+
+
+});
+
+Vue.component('classes_degree', {
+
+  props: ['course','name',],
+  template: `            
+  <section v-if = "course.SUBJ === name && course.EMPHASIS == null">
+  <input  type="checkbox" id="course.CRS" value="true" v-model="course.COMPLETED" >
+  <label for="course.CRS"> {{course.CRS}} {{course.TITLE}} </label>
+  <section v-if = "course.COMPLETED == true" >
+    <select  v-model="course.GRADE">
+      <option :value = "null" disabled selected>Select your Grade</option>
+      <option>Schedule it</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+      <option>Currently Taking</option>
+      </select>            
+    <section v-if = "course.GRADE === 'Schedule it'">
+      <select  v-model="course.SEMESTER">
+        <option :value = "null" disabled selected>Choose Semester</option>
+        <option>First Semester</option>
+        <option>Second Semester</option>
+        <option>Third Semester</option>
+        <option>Fourth Semester</option>
+        <option>Fifth Semester</option>
+        <option>Sixth Semester</option>
+        <option>Seventh Semester</option>
+        <option>Eighth Semester</option>
+        <option>Ninth Semester</option>
+        </select>
+      </section>  
+    </section>
+  <br> 
+  </section>   `,
+});
+
+Vue.component('classes_emphasis', {
+
+  props: ['course','name', 'enumber'],
+  template: `            
+  <section v-if = "course.EMPHASIS == enumber ">
+  <input  type="checkbox" id="course.CRS" value="true" v-model="course.COMPLETED" >
+  <label for="course.CRS"> {{course.CRS}} {{course.TITLE}} </label>
+  <section v-if = "course.COMPLETED == true" >
+    <select  v-model="course.GRADE">
+      <option :value = "null" disabled selected>Select your Grade</option>
+      <option>Schedule it</option>
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+      <option>Currently Taking</option>
+      </select>            
+    <section v-if = "course.GRADE === 'Schedule it'">
+      <select  v-model="course.SEMESTER">
+        <option :value = "null" disabled selected>Choose Semester</option>
+        <option>First Semester</option>
+        <option>Second Semester</option>
+        <option>Third Semester</option>
+        <option>Fourth Semester</option>
+        <option>Fifth Semester</option> 
+        <option>Sixth Semester</option>
+        <option>Seventh Semester</option>
+        <option>Eighth Semester</option>
+        <option>Ninth Semester</option>
+        </select>
+      </section>  
+    </section>
+  <br> 
+  </section>    
+  </template>
+</section>`,
+});
+
+
+
